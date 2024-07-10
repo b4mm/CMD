551ec69b4c6c0a855f25f4e5b7fcd6d7279d2a8c
b4mm <164242963+b4mm@users.noreply.github.com>
2024-07-10 13:07:51 -0400

    AzureVPNTroubleshooter.bat

--- azure_vpn_troubleshooter.bat
+++ azure_vpn_troubleshooter.bat
@@ -7,8 +7,8 @@ ipconfig /flushdns
 
 taskkill /f /IM AzVpnAppx.exe
 
+::Restarts Remote Access Manager service.
 net stop RasMan && net start RasMan
 
-pause
-
+::Next line of instructions might change depending on which version of the Azure VPN Client application is installed. 
 start "C:\Program Files\WindowsApps\Microsoft.AzureVpn_3.3.1.0_x64__8wekyb3d8bbwe\AzVpnAppx.exe"
